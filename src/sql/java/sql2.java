ff0116dc809b1fd2c263cd14f8f9bff4fc239f6b
IN1DM3B_24 <IN1DM3B_24@HZ317750.H015112.NET>
2019-01-17 12:54:47 +0100

    Bandai putos amos

--- src/sql/java/sql2.java
+++ src/sql/java/sql2.java
@@ -1,5 +1,10 @@
 package sql.java;
 
 public class sql2 {
-	System.out.println("Bandai Ratas");
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Bandai Ratas");
+	System.out.println("Bandai putos amos");
+	}
+	
 }
